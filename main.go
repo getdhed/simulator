f6e42c75af448d87e39e6f13435b26bb281b2594
getdhed <nazarzhebrun@gmail.com>
2026-01-27 20:47:56 +0300

    fixed con 2.0

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	con := "postgresql://postgres.bvwrjsxqzvnezeakqjrj:[YOUR-PASSWORD]@aws-1-eu-west-2.pooler.supabase.com:5432/postgres"
+	con := "postgresql://postgres.bvwrjsxqzvnezeakqjrj:DydsbRS3bN1228@aws-1-eu-west-2.pooler.supabase.com:5432/postgres"
 	ctx := context.Background()
 	if err := sql.Connection(ctx, con); err != nil {
 		panic(err)
